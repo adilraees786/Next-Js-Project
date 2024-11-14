af1186cef56d18d43369df65502bc51ceba28e9b
adilraees786 <adilraees911@gmail.com>
2024-11-14 12:55:26 +0500

    addh2

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,9 @@
 export default function () {
   return (
     <div className="mt-4">
-      <div className=" bg-slate-100 items-center flex justify-center">
-        <h1 className=" text-xl font-extrabold text-red-800">Hello world</h1>
+      <div className=" bg-slate-100 ">
+        <h1 className=" text-xl font-extrabold text-red-800 ">Hello world</h1>
+        <h2 className=" text-xl text-yellow-700">Hello Next Js</h2>
       
       </div>
     </div>
